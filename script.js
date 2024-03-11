8736f4acc879f851f6039b6d42245ff5433d7b34
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2024-03-11 09:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const ip = document.querySelector('#newTodoInput');
+const addBtn = document.querySelector('#addTodoBtn');
+const todosList = document.querySelector('#todoList');
 
+addBtn.addEventListener('click', ((e) => {
+	const toDoData = ip.value;
+	if(toDoData != '') {
+		const li = document.createElement('li');
+		li.innerHTML = toDoData;
+		todosList.append(li);
+		ip.value = '';
+	}
+}));
